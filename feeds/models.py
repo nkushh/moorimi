7bc7dc1f978fb0960424ddcf19af8b76bd512678
nkushh <mugunapiero@gmail.com>
2017-11-14 09:00:29 +0300

    Added vaccination models

--- feeds/models.py
+++ feeds/models.py
@@ -3,6 +3,7 @@ from dairy.models import Cattle
 
 # Create your models here.
 class Feeds_inventory(models.Model):
+	account = account = models.ForeignKey('auth.User', default=1, null=True)
 	feed = models.CharField(max_length=200)
 	feed_type = models.CharField(max_length=200)
 	stock = models.FloatField()
@@ -13,11 +14,12 @@ class Feeds_inventory(models.Model):
 		return self.feed
 
 class Feed_consumption(models.Model):
+	account = account = models.ForeignKey('auth.User', default=1, null=True)
 	cattle = models.ForeignKey(Cattle)
 	feed = models.ForeignKey(Feeds_inventory, on_delete=models.CASCADE)
 	amount = models.FloatField()
 	feeding_date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return "{}".format(feed.feed)
+		return self.feed.feed
 
--- health/models.py
+++ health/models.py
@@ -3,6 +3,7 @@ from dairy.models import Cattle
 
 # Create your models here.
 class Health_record(models.Model):
+	account = account = models.ForeignKey('auth.User', default=1, null=True)
 	animal = models.ForeignKey(Cattle)
 	symptoms = models.TextField()
 	diagnosis = models.CharField(max_length=200)
@@ -15,3 +16,18 @@ class Health_record(models.Model):
 
 	def __str__(self):
 		return "{}".format(animal.name)
+
+class Vaccinate_schedule(models.Model):
+	account = account = models.ForeignKey('auth.User', default=1, null=True)
+	vaccine = models.CharField(max_length=200)
+	scheduled_date = models.DateField()
+	status = models.IntegerField(default=0)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.vaccine
+
+class Vaccinated_animals(models.Model):
+	account = account = models.ForeignKey('auth.User', default=1, null=True)
+	vaccine = models.ForeignKey(Vaccinate_schedule)
+	cattle = models.ForeignKey(Cattle)
